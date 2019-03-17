a391b755032355e562870d883873ca4ff2e80633
Pogor2p <Pogor2p@mail.ru>
2019-03-17 18:25:05 +0300

    foot tabs2

---
+++ src/main2.js
@@ -0,0 +1,6 @@
+"use strict";
+
+//  src('node_modules/jquery/dist/jquery.min.js');
+//  src('node_modules/slick-carousel/slick/slick.min.js');
+
+console.log('jffffffffffffffffffffffffffffjjkl;lklk');
